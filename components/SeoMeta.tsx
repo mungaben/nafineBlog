267d6c71a52f9ac47168122a19ed45bbee964947
mungaben <kamaumungai3742@gmail.com>
2023-06-20 12:29:22 +0300

    added sanity

---
+++ components/SeoMeta.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SeoMeta = () => {
+  return (
+    <div>SeoMeta</div>
+  )
+}
+
+export default SeoMeta
--- sanity/UTILS/Posts.ts
+++ sanity/UTILS/Posts.ts
@@ -1,16 +1,30 @@
-import {groq } from 'next-sanity'
-import {client} from '@/sanity/lib/client'
+import { groq } from "next-sanity";
+import { client } from "@/sanity/lib/client";
 
 export const getPosts = async () => {
     const posts = await client.fetch(
-        groq`*[_type == "post"]{
-        _id,
-        title,
-        slug,
-        "author": author-> {
-            _id,
-            name
-        }}
-    `)
-    return posts
-}
+        groq`*[_type == "post"   ]| order(_createdAt desc)
+        {
+ _id,
+ _createdAt,
+      title,
+      "slug": slug.current,
+      author->{name, slug, image},
+      "image": mainImage.asset->url,
+      categories[]->,
+      tags[],
+      comments[]{
+        authorName,
+        content,
+        timestamp
+      },
+      publishedAt,
+      body
+
+        }[0..9]
+          
+       
+    `
+    );
+    return posts;
+};
